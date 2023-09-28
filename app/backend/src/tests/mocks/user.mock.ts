0bb79c6254b7a75a9ea771f3848907666969aee1
Nicolas Lima <nicolaslima1500@gmail.com>
2023-09-28 20:06:43 -0300

    test: cria teste para post /login

---
+++ app/backend/src/tests/mocks/user.mock.ts
@@ -0,0 +1,24 @@
+export const validUserFromDB = {
+  "id": 1,
+  "username": "Admin",
+  "role": "admin",
+  "email": "admin@admin.com",
+  "password": "$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW",
+}
+
+export const userCreate = {
+  "username": "Admin",
+  "role": "admin",
+  "email": "admin@admin.com",
+  "password": "$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW",
+}
+
+export const newUser = {
+  "email": "string",
+  "password": "string"
+}
+
+
+export const mockValidToken = {
+  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjU0NTI3MTg5fQ.XS_9AA82iNoiVaASi0NtJpqOQ_gHSHhxrpIdigiT-fc" // Aqui deve ser o token gerado pelo backend.
+}
---
+++ app/backend/src/tests/users.test.ts
@@ -0,0 +1,59 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import UserModel from '../database/models/UserModel'
+
+import { Response } from 'superagent';
+import { mockValidToken, newUser, userCreate, validUserFromDB } from './mocks/user.mock';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('POST /login', () => {
+  /**
+   * Exemplo do uso de stubs com tipos
+   */
+
+  // let chaiHttpResponse: Response;
+
+  // before(async () => {
+  //   sinon
+  //     .stub(Example, "findOne")
+  //     .resolves({
+  //       ...<Seu mock>
+  //     } as Example);
+  // });
+
+  // after(()=>{
+  //   (Example.findOne as sinon.SinonStub).restore();
+  // })
+
+  // it('...', async () => {
+  //   chaiHttpResponse = await chai
+  //      .request(app)
+  //      ...
+
+  //   expect(...)
+  // });
+
+  beforeEach(function () {
+    sinon.restore()
+  });
+
+  it('Testa se ao fazer uma requisição do tipo POST para a rota /login é possível fazer login com sucesso', async function () {
+    const mockFindOneReturn = UserModel.build(validUserFromDB);
+    const mockCreateReturn = UserModel.build(userCreate);
+
+    sinon.stub(UserModel, "findOne").resolves(mockFindOneReturn);
+    sinon.stub(UserModel, "create").resolves(mockCreateReturn);
+
+    const { status, body } = await chai.request(app).post('/login').send(newUser);
+
+    expect(status).to.be.equal(200);
+    expect(body).to.be.deep.equal(mockValidToken);
+  });
+});

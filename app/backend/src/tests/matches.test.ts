79086bd1abc756dc23587205c6d553bc15aa7975
Nicolas Lima <nicolaslima1500@gmail.com>
2023-10-02 15:00:44 -0300

    test: modifica teste para a rota get /matches

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -4,11 +4,12 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import MatchesModel from '../database/models/MatchesModel'
+import MatcheModel from '../database/models/MatcheModel'
 import { teamsFromDB } from './mocks/team.mocks'
 
 import { Response } from 'superagent';
-import { matchesFromDb } from './mocks/matches.mock';
+import { matchesFromDb } from './mocks/matche.mock';
+import TeamModel from '../database/models/TeamModel';
 
 chai.use(chaiHttp);
 
@@ -19,12 +20,17 @@ describe('GET /matches', () => {
     sinon.restore()
   });
 
-  it('Testa se ao fazer uma requisição do tipo GET para a rota /matches é retornado um array de partidas', async function () {
-    const mockFindAllReturn = MatchesModel.bulkBuild(matchesFromDb);
-    sinon.stub(MatchesModel, "findAll").resolves(mockFindAllReturn);
+  it('Testa se ao fazer uma requisição do tipo GET para a rota /matches é retornado um array de partidas', async function () {    
+    const mockModel = sinon.stub(MatcheModel, "findAll").resolves(matchesFromDb as unknown as MatcheModel[]);
 
     const { status, body } = await chai.request(app).get('/matches');
 
+    expect(mockModel.calledWithExactly({
+      include: [
+        { model: TeamModel, as: 'homeTeam', attributes: { exclude: ['id'] } },
+        { model: TeamModel, as: 'awayTeam', attributes: { exclude: ['id'] } },
+      ]
+    })).to.be.true
     expect(status).to.be.equal(200);
     expect(body).to.be.deep.equal(matchesFromDb);
   });

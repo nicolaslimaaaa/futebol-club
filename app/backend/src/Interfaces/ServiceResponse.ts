a6ad41a9227d986e40226728399e8223959b5284
Nicolas Lima <nicolaslima1500@gmail.com>
2023-10-04 18:38:11 -0300

    refactor: refatora a interface ServiceResponse

--- app/backend/src/Interfaces/ServiceResponse.ts
+++ app/backend/src/Interfaces/ServiceResponse.ts
@@ -2,7 +2,7 @@ import { Code } from '../utils/mapStatusHTTP';
 
 export default interface ServiceResponse<T> {
   status: Code,
-  data: { message: string } | T
+  data: T
 }
 
 export interface LoginResponse {
